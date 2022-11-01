72b8477ff5646bdd6ec3b99901502e77e299497d
majim3 <45204846+majim3@users.noreply.github.com>
2022-11-01 15:01:15 +0200

    add basic dummy routeting

--- week-two/app.js
+++ week-two/app.js
@@ -11,4 +11,21 @@ app.get('/user', (req, res) => {
   res.send('From this endpoint you can get users.')
 });
 
+app.get('/cat/:catId',(req,res) =>{
+  console.log(req.params);
+  res.send('From this endpoint you can get cats with id ' + req.params.catId)
+  });
+
+app.post('/cat',(req,res) =>{
+//console.log(req);
+res.send('From this endpoint you can add more cats')
+});
+
+app.put('/cat', (req, res) => {
+  res.send('From this endpoint you can put users.');
+});
+app.delete('/cat', (req, res) => {
+  res.send('From this endpoint you can delete users.');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
