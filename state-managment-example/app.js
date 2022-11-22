af3fdac95d059c5a486b30589fcf8e654aa501ba
majim3 <45204846+majim3@users.noreply.github.com>
2022-11-22 16:53:18 +0200

    session task done

--- state-managment-example/app.js
+++ state-managment-example/app.js
@@ -1,73 +1,69 @@
 'use strict';
 const express = require('express');
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
-const dummyUser ={
+const dummyUser = {
   username: 'foo',
   password: 'bar'
-}
+};
+// dont do this
 let loggedIn = false;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
-app.use(cookieParser())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for form data
 app.use(session({
-secret: 'hhyjhhghnbgggtrdgtr5tgtytkjhmhmjuuyjuyjhhfgfgg', 
-saveUninitialized: false,
-resave: true,
-cookie: {maxAge: 60000}
-
+  secret: 'bhnjnhgfgvhbhghhgyttytytt',
+  saveUninitialized: false,
+  resave: true,
+  cookie: {maxAge: 60000}
 }));
 
+
 app.get('/', (req, res) => {
-  res.render('home'); 
+  res.render('home');
 });
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+app.get('/secret', (req, res) => {
+  if (req.session.loggedIn) {
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+});
+app.post('/login', (req, res) => {
+  
+  if (req.body.username == dummyUser.username && req.body.password == dummyUser.password){
+   
+    req.session.loggedIn = true;
+  } 
+  res.redirect('/secret');
+});
+app.get('/logout', (req, res) => {
+  req.session.loggedIn = false;
+  res.redirect('/');
+});
+
 
 app.get('/getCookie', (req, res) => {
-  console.log(req.cookies)
-  res.send('your color choise was: ' + req.params.color);
+  console.log(req.cookies);
+  res.send('your color choice was: ' + req.cookies.color);
 });
-
-app.get('/form', (req, res) => {
- res.render('form')
-});
-
-app.get('/secret', (req, res) => {
-  if(req.session.loggedIn){
-    res.render('secret')
-  }else{
-    res.redirect('/form')
-  }
- });
-
- app.post('/login', (req, res) => {
-  //check username and password if they match
-  console.log(req.params.session)
-  if(req.body.username == dummyUser.username && req.body.password == dummyUser.password){
-    loggedIn = true
-    req.session.loggedIn = true
-  }
-});
-
-  app.get('/logout', (req, res) => {
-    session.loggedIn = false;
-   // res.clearCookie('connect.sid')
-  res.redirect('/')
-    });
-
 app.get('/deleteCookie', (req, res) => {
-res.clearCookie('color')
-res.send("color cookie deleted ")
+  res.clearCookie('color');
+  res.send('Color cookie deleted.');
+});
+app.get('/setCookie/:color', (req, res) => {
+  console.log(req.params.color);
+  res.cookie('color', req.params.color);
+  res.send('cookie set');
 });
 
-app.get('/setCookie/:color', (req,res)=>{
-  console.log(req.params.color)
-  res.cookie('color', req.params.color)
-  res.send('cookie set')
-});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
