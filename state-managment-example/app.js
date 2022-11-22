f79984acedde9c512c4192564b8fd36756dfdac7
majim3 <45204846+majim3@users.noreply.github.com>
2022-11-22 15:07:36 +0200

    login

--- state-managment-example/app.js
+++ state-managment-example/app.js
@@ -1,11 +1,28 @@
 'use strict';
 const express = require('express');
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const app = express();
 const port = 3000;
 
+const dummyUser ={
+  username: 'foo',
+  password: 'bar'
+}
+let loggedIn = false;
+
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+secret: 'hhyjhhghnbgggtrdgtr5tgtytkjhmhmjuuyjuyjhhfgfgg', 
+saveUninitialized: false,
+resave: true,
+cookie: {maxAge: 60000}
+
+}));
 
 app.get('/', (req, res) => {
   res.render('home'); 
@@ -15,6 +32,33 @@ app.get('/getCookie', (req, res) => {
   console.log(req.cookies)
   res.send('your color choise was: ' + req.params.color);
 });
+
+app.get('/form', (req, res) => {
+ res.render('form')
+});
+
+app.get('/secret', (req, res) => {
+  if(req.session.loggedIn){
+    res.render('secret')
+  }else{
+    res.redirect('/form')
+  }
+ });
+
+ app.post('/login', (req, res) => {
+  //check username and password if they match
+  console.log(req.params.session)
+  if(req.body.username == dummyUser.username && req.body.password == dummyUser.password){
+    loggedIn = true
+    req.session.loggedIn = true
+  }
+});
+
+  app.get('/logout', (req, res) => {
+    session.loggedIn = false;
+   // res.clearCookie('connect.sid')
+  res.redirect('/')
+    });
 
 app.get('/deleteCookie', (req, res) => {
 res.clearCookie('color')
